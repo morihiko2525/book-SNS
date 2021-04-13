78424602e31b84261266426a29f0db7652d83676
morihiko2525 <morihiko223@gmail.com>
2021-04-13 15:40:43 +0900

    bootstrapTest

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
     </head>
     <body>
         <p>IT WORKS!</p>
+        <div class = "form-group">
+        <label class="col-sm-2 control-label">項目1</label>
+          <div class="col-sm-10">
+            <input class="form-control" type="text">
+          </div>
+        </div>
         <?php print"a";?>
     </body>
 </html>
