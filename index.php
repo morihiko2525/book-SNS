0b1af04394ae1e20ba9142f68342a6f2a9d8b40f
morihiko2525 <morihiko223@gmail.com>
2021-04-13 15:30:47 +0900

    .phpに変更

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+        <title>テストページです</title>
+    </head>
+    <body>
+        <p>IT WORKS!</p>
+    </body>
+</html>
